d5f3e2cb4bd78dd81868d61fa1d039e215b20fbe
ellisrourkePC <ellisrourke@gmail.com>
2019-09-26 17:44:12 +1000

    getting data

--- main.cpp
+++ main.cpp
@@ -4,13 +4,28 @@
 #include <map>
 #include <unordered_map>
 #include <chrono>
+typedef std::pair<std::string,std::string> nameSub;
+struct hashFunc{
+    size_t operator()(const nameSub &test) const{
+        std::cout << (std::hash<std::string>()(test.first) * std::hash<std::string>()(test.second)) << std::endl;
+        return std::hash<std::string>()(test.first) * std::hash<std::string>()(test.second);
+    }
+
+    /*bool operator==(const nameSub &test , const nameSub &test2) const
+    {
+        return test.first == test2.first && test.second == test2.second;
+    }*/
+};
+
 int main(){
     auto start = std::chrono::steady_clock::now();
     clock_t startClock = clock(); // record the clock at start
 
-    std::map<std::pair<std::string,std::string>, int> students;
-    students.insert(std::make_pair(std::make_pair("Ellis", "Mathematics"),98));
-    std::map<std::pair<std::string,std::string>, int>::iterator itr;
+    //std::map<std::pair<std::string,std::string>, int> students;
+    //std::map<std::pair<std::string,std::string>, int>::iterator itr;
+
+    std::unordered_map<nameSub,int,hashFunc> students;
+    std::unordered_map<nameSub,int,hashFunc>::iterator itr;
 
     std::string line;
     std::stringstream test;
@@ -22,7 +37,9 @@ int main(){
         int grade;
         std::cin >> name >> subject >> grade;
         //std::cout << name << subject << grade << std::endl;
-        students.insert(std::make_pair(std::make_pair(name, subject),grade));
+        //students.insert(std::make_pair(std::make_pair(name, subject),grade));
+        std::string agg = name + subject;
+        students[nameSub(name,subject)] = grade;
     }
 
     //std::map<std::string,std::string> students;
@@ -31,9 +48,13 @@ int main(){
     //std::map<std::string, std::string>::iterator itr;
 
     for (itr = students.begin(); itr != students.end(); ++itr) {
-       std::cout  << itr->first.first  << " " <<   itr->first.second  << " " << itr->second  << std::endl;
+       std::cout  << itr->first.first  << " " <<   itr->first.second  << " " << itr->second << "From Map" << std::endl;
     }
     std::cout << std::endl;
+
+
+
+
 
     auto end = std::chrono::steady_clock::now();
     std::cout << "Elapsed time in milliseconds : "
